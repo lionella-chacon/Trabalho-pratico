48eb560b595605cd8bab5826fcde9739a9d109a1
lionella-chacon <lionellachacon26@gmail.com>
2024-09-26 20:13:18 -0300

    Add files via upload
    DGT2817/Lógica, Algoritmos e Programação de Computadores

---
+++ estruturas_condicao2.py
@@ -0,0 +1,10 @@
+tempoExperiencia = 3
+
+if tempoExperiencia == 3<2:
+    print('Nível de conhecimento júnior.')
+
+elif tempoExperiencia == 3>2<5:
+    print('Nível de conhecimento pleno.')
+
+else:
+    print('Nível de conhecimento sênior.')
---
+++ estruturas_repeticao1.py
@@ -0,0 +1,6 @@
+entrada_idade = ''
+
+while entrada_idade != 0:
+    entrada_idade = int(input('Digite um número qualquer ou 0 para sair: '))
+
+print(f'Número digitado: {entrada_idade}')
